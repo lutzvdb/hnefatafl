d04eea31c7ea332ad56008171426b8356db80f8f
Lutz von der Burchard <lvdb@gmx.net>
2022-11-13 14:58:56 +0100

    Title error fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,23 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Game from '../components/game'
 
 const Home: NextPage = () => {
   const [bgColor, setBgColor] = useState("bg-red-50")
   const [titleAppendix, setTitleAppendix] = useState('')
+  const [fullTitle, setFullTitle] = useState('hnefatafl')
 
   const classNames = "h-screen w-screen " + bgColor
+
+  useEffect(() => {
+    setFullTitle('hnefatafl' +titleAppendix)
+  }, [titleAppendix])
 
   return (
     <>
       <Head>
-        <title>hnefatafl {titleAppendix}</title>
+        <title>{fullTitle}</title>
       </Head>
       <div className={classNames}>
         <Game setBgColor={setBgColor} setTitleAppendix={setTitleAppendix} />
